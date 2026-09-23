56ea34187d24f10bd23a6eece56a4e421a96455f
mkrwii <mkrwii@live.com>
2026-09-23 09:49:09 +0200

    added rudimentary scenario testing

--- tests.py
+++ tests.py
@@ -1,3 +1,34 @@
+import data_generator as dg
+from classes import *
+
+scenarios = dg.available_scenarios()
+
+def test():
+    for scenario in scenarios:
+        run_scenario(scenario)
+
+def run_scenario(scenario):
+    print(f"Running scenario: {scenario}")
+    profile, observations = dg.generate_fitness_data(
+        participant_id="P123",
+        scenario=scenario,
+        seed=2123,
+        number_of_windows=12,
+    )
+
+    print("\n=== Participant Profile ===")
+    for key, value in profile.items():
+        print(f"{key}: {value}")
+
+        clean_observations = []
+
+    for obs in observations:
+        try:
+            obs = Observation(**obs)   # map dict → object
+            clean_observations.append(obs)
+        except ValueError as e:
+            print(e)
+
 if __name__ == "__main__":
-    # Your main code logic here
+    test()
     print("Hello from tests.py!")
